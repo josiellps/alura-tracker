79682ab0db0fe6edc0518496a2b699470eda4435
josiellps <josiel_lps@hotmail.com>
2022-04-21 17:52:36 -0300

    Alura Tracker

---
+++ src/components/Box.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Box",
+  data() {
+    return {
+      estilos: {
+        backgroundColor: "#faf0ca",
+      },
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,31 @@
+<template>
+  <Box>
+    <div class="columns">
+      <div class="column is-7">{{tarefa.descricao|| 'Tarefa sem descrição'}}</div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
+</template>
+<script lang="ts">
+import ITarefa from "@/interfaces/ITarefa";
+import { defineComponent, PropType } from "vue";
+import Cronometro from "./Cronometro.vue";
+import Box from './Box.vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tarefa",
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
+  },
+  components: {
+    Cronometro,
+    Box
+  },
+});
+</script>
